4694df22fbae398c3cf19979f224c9a5cf0ff85b
rotaru-madalina <r.madalina0@gmail.com>
2024-11-13 20:01:04 +0200

    Made "discover albums" page
    each album has a button "vezi melodii" which displays all album's songs

---
+++ BoolToTextConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDMProiect
+{
+    public class BoolToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue && parameter is string text)
+            {
+                var texts = text.Split('|');
+                return boolValue ? texts[1] : texts[0];
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
---
+++ DaoAlbum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace PDMProiect
+{
+    public class DaoAlbum
+    {
+        SQLiteConnection conn;
+
+        public DaoAlbum()
+        {
+            string cale = Path.Combine(FileSystem.AppDataDirectory, "album.db");
+            conn = new SQLiteConnection(cale);
+            conn.CreateTable<Album>();
+        }
+        public bool AlbumExists(string name, int id)
+        {
+            return conn.Table<Album>()
+                       .Any(a => a.AlbumName == name && a.Id == id);
+        }
+        public int InsertAll(List<Album> albums)
+        {
+            int insertedCount = 0;
+            foreach (var album in albums)
+            {
+                if (!AlbumExists(album.AlbumName, album.Id))
+                {
+                    conn.Insert(album);
+                    insertedCount++;
+                }
+            }
+            return insertedCount;
+        }
+    }
+}
